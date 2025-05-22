9f4148c48b44a39542963283e7e7ac1f1aa4b573
rish27c <rish27c@gmail.com>
2025-05-22 19:07:52 +0530

    Add files via upload

---
+++ Berminal.py
@@ -0,0 +1,17 @@
+try:
+    import Berminal
+    Berminal.Berminal()
+except:
+    print('It seems that the Berminal is unable to enter backup-mode.')
+    print('Entering Emergency recovery mode...')
+    import os
+    import shutil
+    try:
+        print('Recovering file...')
+        url='https://github.com/rish27c/Berminal.git'
+        os.system(f'git clone {url}')
+        shutil.move(os.path.join(os.path.join(os.path.dirname(__file__), 'Berminal'), 'Berminal_box'), os.path.join(os.path.dirname(__file__), 'Berminal_recovery'))
+        shutil.rmtree(os.path.join(os.path.join(os.path.dirname(__file__), 'Berminal')))
+        shutil.move(os.path.join(os.path.dirname(__file__), 'Berminal_recovery'), os.path.join(os.path.dirname(__file__), 'Berminal'))
+    except:
+        print('Berminal(Emergency Recovery)>>Error!! Please install -git manually.')
